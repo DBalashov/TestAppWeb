a22b3c9bee72adba371c50a458901196a8946212
denisio <denisio@tk-chel.ru>
2022-01-12 23:21:14 +0500

    initial

---
+++ TestAppWeb/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TestAppWeb.Models;
+
+namespace TestAppWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var envs = Environment.GetEnvironmentVariables();
+        var r    = new Dictionary<string, string>();
+        foreach (var key in envs.Keys.OfType<string>())
+            r.Add(key, envs[key]!.ToString()!);
+        return View(r);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
